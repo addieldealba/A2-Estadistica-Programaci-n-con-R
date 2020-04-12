34686956f5f7330fe74dc14d29b037a5a37adcd4
adealbas <addielu@gmail.com>
2020-04-11 20:30:40 -0500

    Update Solucion_Reto02.R

--- Sesion-02/Reto-02/Solucion_Reto02.R
+++ Sesion-02/Reto-02/Solucion_Reto02.R
@@ -12,3 +12,7 @@ evaluate <- function(func, dat){
   # Remember: the last expression evaluated will be returned! 
   func(dat)
 }
+
+evaluate(sd,c(1.4, 3.6, 7.9, 8.8))
+evaluate(function (x) {x[1]},c(8,4,0))
+evaluate(function (x) {x[length(x)]},c(8,4,0))
