61107c1679503c53022110fef4af278a1a7ea1d6
adealbas <addielu@gmail.com>
2020-04-11 23:59:54 -0500

    Update Solucion_Ejemplo03.R

--- Sesion-02/Ejemplo-03/Solucion_Ejemplo03.R
+++ Sesion-02/Ejemplo-03/Solucion_Ejemplo03.R
@@ -11,6 +11,18 @@ telegram <- function(...){
   paste("START",...,"STOP")
 }
 
+mad_libs <- function(...){
+  # Do your argument unpacking here!
+  args <- list(...)
+  place <- args[[1]]
+  adjective  <- args[[2]]
+  noun  <- args[[3]]
+  # Don't modify any code below this comment.
+  # Notice the variables you'll need to create in order for the code below to
+  # be functional!
+  paste("News from", place, "today where", adjective, "students took to the streets in protest of the new", noun, "being installed on campus.")
+}
+
 
 
 
